051c932d7677aabd9c62cc4c703d398b7629b596
Develer12 <cenia-v@mail.ru>
2019-10-22 21:01:09 +0300

    Non completed, problems with body

--- Lab9/client.js
+++ Lab9/client.js
@@ -1,24 +1,51 @@
 let http = require('http');
 let query = require('querystring');
 
-let path = `/getinfo`;
 
+let path = `/getinfo`;
+let param = ' ';
 let options =
 {
   host:'localhost',
-  path:path,
+  path: path,
   port: 5000,
   method:'GET'
 };
-
-const req = http.request(options, (res) =>
+const req1 = http.request(options, (res) =>
 {
-    console.log(`Method: ${req.method}`);
+    console.log('------------TASK1------------');
+    console.log(`Method: ${req1.method}`);
     console.log(`StatusCode: ${res.statusCode}`);
-    console.log(`StatusMessage: ${req.statusMessage}`);
-    console.log(`RemoteAddress: ${req.connection.remoteAddress}`);
-    console.log(`RemotePort: ${req.connection.remotePort}`);
-    console.log('Body: ' + JSON.stringify(res.data));
+    console.log(`StatusMessage: ${res.statusMessage}`);
+    console.log(`RemoteAddress: ${req1.connection.remoteAddress}`);
+    console.log(`RemotePort: ${req1.connection.remotePort}`);
+    console.log('Body: ' + JSON.stringify(res.body));
 });
 
-req.end();
+
+param = query.stringify({x:3, y: 4});
+options.path = `/xy?${param}`;
+
+const req2 = http.request(options, (res) =>
+{
+    console.log('------------TASK2------------');
+    console.log('Status: ' + res.statusCode + '. ' + res.statusMessage);
+    console.log('Parameters: ' + JSON.stringify(res.data));
+});
+
+
+param = query.stringify({x:3, y: 4, s: 'Dada'});
+options.path = `/xys?${param}`;
+options.method = 'POST';
+
+const req3 = http.request(options, (res) =>
+{
+    console.log('------------TASK3------------');
+    console.log('Status: ' + res.statusCode + '. ' + res.statusMessage);
+    console.log('Parameters: ' + JSON.stringify(res.data));
+});
+
+
+req1.end();
+req2.end();
+req3.end();
--- Lab9/index.js
+++ Lab9/index.js
@@ -1,16 +1,27 @@
 var url = require("url");
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
 
 const PORT = 5000;
 const HOST = 'localhost';
 
 app.get('/getinfo', (req, res) => {
     res.statusCode = '201';
-    res.statusMessage = "Current password does not match";
+    res.statusMessage = "Sended";
     res.json({data: "Dada ya"});
 
+});
+
+app.get('/xy', (req, res) => {
+    res.json({result: 'success: ' + req.query.x + req.query.y});
+});
+
+app.post('/xys', (req, res) => {
+    res.json({result: req.body.s + ': ' + req.body.x + req.body.y});
 });
 
 const server = app.listen(PORT, HOST, () => {
