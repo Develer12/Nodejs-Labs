cf6a41af0f5d6bd4ba457b587cb38984a99a3cf3
Develer12 <cenia-v@mail.ru>
2019-11-26 14:54:10 +0300

    Started

--- Lab14/index.js
+++ Lab14/index.js
@@ -1,12 +1,12 @@
 const sql = require('mssql');
 const express = require('express');
 var bodyParser = require('body-parser')
-const que = require('./que');
+const que = require('./queries');
 
 
 const PORT = 3000;
 const HOST = 'localhost';
-let config = {user: 'DESKTOP-U4BLHC6', password: '', server: 'localhost', database: 'Nodejs'};
+let config = {user: 'DESKTOP-U4BLHC6', server: 'localhost', database: 'Nodejs'};
 
 
 const app = express();
@@ -17,6 +17,35 @@ const server = app.listen(PORT, HOST, () =>
     console.log('Listening on ' + URL);
 })
 .on('error', (e) => {console.log(`${URL} | error: ${e.code}`)});
+
+let connection=(Query)=>
+{
+  sql.connect(config, err =>
+  {
+
+       console.log("DB is connected");
+       Query;
+
+  })
+}
+
+let getpul = ()=>{new sql.Request().query('select PULPIT_NAME from PULPIT', _result)};
+
+let _result = (err, result) =>
+{
+  console.log('Start Output');
+  if(err) console.log('Error: ', err.code);
+  else
+  {
+    for(let i = 0; i<result.rowsAffected[0]; i++)
+    {
+      let str = '--';
+      for(key in result.recordset[i])
+        str += `${key} = ${result.recordset[i][key]}`;
+      console.log(str);
+    }
+  }
+}
 
 
 app.get('/', (req, res) =>
@@ -31,7 +60,8 @@ app.get('/api/faculties', (req, res) =>
 
 app.get('/api/pulpits', (req, res) =>
 {
-
+    console.log('Get pulpits');
+    connection(getpul());
 });
 
 app.get('/api/subjects', (req, res) =>
